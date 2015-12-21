05dcfae7928d36f9a390062f96f97385dcc0e09f
Martin Sosa <mr.martinsosa@gmail.com>
2015-12-21 13:29:41 -0500

    Created the "pongs" and realigned positions.

--- AusumPong.rb
+++ AusumPong.rb
@@ -12,18 +12,43 @@ class Ball
 
   def draw
     @window.draw_quad(
-      10, 10, Color::BLUE,
-      20, 10, Color::BLUE,
-      20, 20, Color::BLUE,
-      10, 20, Color::BLUE,
+      Game::WIDTH/2-5, Game::HEIGHT/2-5, Color::BLUE,
+      Game::WIDTH/2+5, Game::HEIGHT/2-5, Color::BLUE,
+      Game::WIDTH/2+5, Game::HEIGHT/2+5, Color::BLUE,
+      Game::WIDTH/2-5, Game::HEIGHT/2+5, Color::BLUE,
+    )
+  end
+end
+
+class Player
+  def initialize(window, x, y)
+    @window = window
+    @x = x
+    @y = y
+  end
+
+  def update
+
+  end
+
+  def draw
+    @window.draw_quad(
+        @x-5, @y-40, Color::BLUE,
+        @x+5, @y-40, Color::BLUE,
+        @x+5, @y+40, Color::BLUE,
+        @x-5, @y+40, Color::BLUE,
     )
   end
 end
 
 class Game < Window
+  WIDTH = 640
+  HEIGHT = 480
   def initialize
-    super(640, 480, false)
+    super(WIDTH, HEIGHT, false)
     @ball = Ball.new(self)
+    @player_1 = Player.new(self, 40, HEIGHT/2)
+    @player_2 = Player.new(self, WIDTH-40, HEIGHT/2)
   end
 
   def update
@@ -32,6 +57,8 @@ class Game < Window
 
   def draw
     @ball.draw
+    @player_1.draw
+    @player_2.draw
   end
 end
 
