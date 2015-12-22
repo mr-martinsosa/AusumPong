a6d7932931d1a3dcecefd7e1a9ccbb7deaa1a0de
Martin Sosa <mr.martinsosa@gmail.com>
2015-12-22 17:21:03 -0500

    Created proper movement for the ball and created the Out of Bounds area.

--- AusumPong.rb
+++ AusumPong.rb
@@ -2,25 +2,40 @@ require 'gosu'
 include Gosu
 
 class Ball
+  attr_accessor :x, :y
   def initialize(window)
     @window = window
+    @x = Game::WIDTH/2
+    @y = Game::HEIGHT/2
+    @vx = 5
+    @vy = 0
   end
 
   def update
-
+    @x += @vx
+    @y += @vy
+    if @y > Game::HEIGHT || @y < 0
+      @vy *= -1
+    end
   end
 
   def draw
     @window.draw_quad(
-      Game::WIDTH/2-5, Game::HEIGHT/2-5, Color::BLUE,
-      Game::WIDTH/2+5, Game::HEIGHT/2-5, Color::BLUE,
-      Game::WIDTH/2+5, Game::HEIGHT/2+5, Color::BLUE,
-      Game::WIDTH/2-5, Game::HEIGHT/2+5, Color::BLUE,
+      @x-5, @y-5, Color::BLUE,
+      @x+5, @y-5, Color::BLUE,
+      @x+5, @y+5, Color::BLUE,
+      @x-5, @y+5, Color::BLUE,
     )
+  end
+
+  def switch_direction(place_on_paddle)
+    @vx *= -1
+    @vy = place_on_paddle / 10
   end
 end
 
 class Player
+  attr_accessor :x, :y
   HEIGHT = 80
   WIDTH = 10
   def initialize(window, x, y)
@@ -41,6 +56,10 @@ class Player
   def move_down
     @y += 5
     @y = [Game::HEIGHT - HEIGHT/2, @y].min
+  end
+
+  def hits?(ball)
+    (@x - ball.x).abs < 5 && (@y - ball.y).abs < 50
   end
 
   def draw
@@ -76,6 +95,15 @@ class Game < Window
     if button_down?(KbDown)
       @player_2.move_down
     end
+
+    if @player_1.hits?(@ball)
+      @ball.switch_direction(@ball.y - @player_1.y)
+    end
+
+    if @player_2.hits?(@ball)
+      @ball.switch_direction(@ball.y - @player_2.y)
+    end
+    @ball.update
   end
 
   def draw
