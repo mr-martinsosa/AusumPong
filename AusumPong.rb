7712461fc541aafd1ac422558b752d4ac61eb8b1
Martin Sosa <mr.martinsosa@gmail.com>
2015-12-22 16:51:46 -0500

    Added movement to "pongs"

--- AusumPong.rb
+++ AusumPong.rb
@@ -21,6 +21,8 @@ class Ball
 end
 
 class Player
+  HEIGHT = 80
+  WIDTH = 10
   def initialize(window, x, y)
     @window = window
     @x = x
@@ -31,12 +33,22 @@ class Player
 
   end
 
+  def move_up
+    @y -= 5
+    @y = [HEIGHT/2, @y].max
+  end
+
+  def move_down
+    @y += 5
+    @y = [Game::HEIGHT - HEIGHT/2, @y].min
+  end
+
   def draw
     @window.draw_quad(
-        @x-5, @y-40, Color::BLUE,
-        @x+5, @y-40, Color::BLUE,
-        @x+5, @y+40, Color::BLUE,
-        @x-5, @y+40, Color::BLUE,
+        @x-WIDTH/2, @y-HEIGHT/2, Color::BLUE,
+        @x+WIDTH/2, @y-HEIGHT/2, Color::BLUE,
+        @x+WIDTH/2, @y+HEIGHT/2, Color::BLUE,
+        @x-WIDTH/2, @y+HEIGHT/2, Color::BLUE,
     )
   end
 end
@@ -52,7 +64,18 @@ class Game < Window
   end
 
   def update
-
+    if button_down?(KbW)
+      @player_1.move_up
+    end
+    if button_down?(KbS)
+      @player_1.move_down
+    end
+    if button_down?(KbUp)
+      @player_2.move_up
+    end
+    if button_down?(KbDown)
+      @player_2.move_down
+    end
   end
 
   def draw
